ae15d93323d6edbb71e7a00c25c7140342f38489
oxfos <rinaldo_bertossa@hotmail.com>
2021-12-04 20:47:34 +0100

    Code iterates over more html files

--- src.py
+++ src.py
@@ -1,39 +1,53 @@
 from bs4 import BeautifulSoup
 import csv  
+import requests
+
+"""
+Used variables:
+coinPages: list containing html links to coin forecast pages
+"""
 
 # Get all websites with predictions
 with open('Long-Term Price Predictions 2021-2031.html', 'r') as fr:
     # read and parse content of coins_list
     coinsHtml = BeautifulSoup(fr, 'lxml')
 
-# get the list of links
+# get the list of link to the coins forecast pages
 links = coinsHtml.main.div.div.find_all('a')
+coinPages = []
 for link in links:
-    print(link.get('href'))
+    coinPages.append(link.get('href'))
+
+# Get the content of the page
+coinPages = ['https://coinpriceforecast.com/bitcoin-forecast-2020-2025-2030','https://coinpriceforecast.com/ethereum-forecast-2020-2025-2030']
 
 
-
-# Per link get the  coint forecast table
-
-with open('BITCOIN PRICE PREDICTION 2021 - 2025 - 2030.html', 'r') as fr:
-    # read and parse content of html_file
-    soup = BeautifulSoup(fr, 'lxml')
-
+# Per link get the coin forecast table
 # prepare list to host data
 data = []
-# find all table rows
-tableRows = soup.find_all('tr')
-# for each table row
-for tr in tableRows:
-    rowList = []
-    rowData = tr.find_all('td')
-    for td in rowData:
-        rowList.append(td.text)
-    data.append(rowList)
 
-# Add title to data
-title = soup.find('h1')
-data[0][3] = title.text.split()[0]
+for link in coinPages:
+    requests.get(link)
+    """
+    with open(link, 'r') as fr:
+        # read and parse content of html_file
+        soup = BeautifulSoup(fr, 'lxml')
+    """
+    soup = BeautifulSoup(requests.get(link).text, 'lxml')
+    # Add title row to data with crypto name
+    first_row = 'Year,Mid-Year,Year-End,'
+    title = soup.find('h1')
+    data.append([first_row + title.text.split()[0]])
+    # for each table row in all table rows get the data
+    trows = soup.find_all('tr')
+    trows.pop(0)
+    for tr in trows:
+        rowList = []
+        for td in tr.find_all('td'):
+            rowList.append(td.text)
+        data.append(rowList)
+
+
 
 # Adding data to csv file
 with open('quotes.csv', 'w', encoding='UTF8', newline='') as f:
