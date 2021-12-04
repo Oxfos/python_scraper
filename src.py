e8bde5296c0c1f95b6ae90a4c8db878832237060
oxfos <rinaldo_bertossa@hotmail.com>
2021-12-04 19:23:09 +0100

    Code to parse one html file and save as csv

--- src.py
+++ src.py
@@ -1,8 +1,36 @@
 from bs4 import BeautifulSoup
+import csv  
+
+# Get all websites with predictions
+
+
 
 with open('BITCOIN PRICE PREDICTION 2021 - 2025 - 2030.html', 'r') as html_file:
+    # read content of html_file
     content = html_file.read()
-    
-    soup = BeautifulSoup(content, 'lxml')
-    tr = soup.find_all('tr')
-    print(tr)
+
+# parse content of content
+soup = BeautifulSoup(content, 'lxml')
+
+# prepare list to host data
+data = []
+# find all table rows
+tableRows = soup.find_all('tr')
+# for each table row
+for tr in tableRows:
+    rowList = []
+    rowData = tr.find_all('td')
+    for td in rowData:
+        rowList.append(td.text)
+    data.append(rowList)
+
+# Add title to data
+title = soup.find('h1')
+data[0][3] = title.text.split()[0]
+
+# Adding data to csv file
+with open('quotes.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the data
+    writer.writerows(data)
