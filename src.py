d1ee2dbc68455ec128b265dd9264af92258112ae
oxfos <rinaldo_bertossa@hotmail.com>
2021-12-04 17:44:52 +0100

    Added src.py

--- src.py
+++ src.py
@@ -1,1 +1,8 @@
 from bs4 import BeautifulSoup
+
+with open('BITCOIN PRICE PREDICTION 2021 - 2025 - 2030.html', 'r') as html_file:
+    content = html_file.read()
+    
+    soup = BeautifulSoup(content, 'lxml')
+    tr = soup.find_all('tr')
+    print(tr)
